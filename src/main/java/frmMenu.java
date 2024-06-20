9bfde546ab2099eaf7208f6909552c06ef6713c6
Leonardo Montes <leonardo.montesm@gmail.com>
2024-06-19 20:54:32 -0500

    #4 Agregar las interfaces iniciales del proyecto

---
+++ src/main/java/frmMenu.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class frmMenu {
+    private JButton registrarVictimaButton;
+    private JButton cifrarArchivoButton;
+    private JButton consulltarGPTButton;
+    private JTextArea textArea1;
+    private JTextPane menuDeIngresoTextPane;
+}
