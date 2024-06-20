7fb007f3c82f39643f8066cfd20fe6f8ac2f227d
Leonardo Montes <leonardo.montesm@gmail.com>
2024-06-19 21:28:45 -0500

    Agregar formulario

---
+++ src/main/java/frmCifradoArchivos.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class frmCifradoArchivos {
+    private JTextField menuDeCiffradoDeTextField;
+    private JButton seleccioneArchivoButton;
+    private JButton cifrarButton;
+    private JButton descifrarButton;
+}
